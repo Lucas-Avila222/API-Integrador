d6f20488145bf6a26ed1026c1f08532c71e0920d
Lucas Ávila <lucasdeavila2008@gmail.com>
2025-04-11 15:14:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,85 +21,129 @@ import express from 'express';
 
 
   app.get("/funcionarios", async (req, res) => {
+    try {
     const { id } = req.params;
     const funcionarios = await mysql2.select("*").from("funcionarios");
     res.json(funcionarios);
+    } catch (error) {
+  console.log("Erro ao buscar funcionário:", error);
+  res.status(404).json({ mensagem: "Funcionário não encontrado" });
+  }
   });
 
     app.post('/funcionarios', async (req, res) => {
     const { nome, cargo, salario } = req.body;
-    const [id] = await bancoDeDados("funcionarios").insert({imagen, nome_completo, nascimento, e_mail, cargo, telefone});
+    const [id] = await mysql2("funcionarios").insert({imagen, nome_completo, nascimento, e_mail, cargo, telefone});
     console.log('Funcionário cadastrado:', {imagen, nome_completo, nascimento, e_mail, cargo});
-    res.status(201).json({ mensagem: 'Funcionário cadastrado com sucesso!' });
+    res.status(201).json({ mensagem: 'Funcionário cadastrado com sucesso' });
 });
 
     app.post('/clientes', async (req, res) => {
     const { nome, email, telefone } = req.body;
-    const [id] = await bancoDeDados("funcionarios").insert({imagen, nome, nascimento, e_mail, telefone, senha_cliente});
+    const [id] = await mysql2("clientes").insert({imagen, nome, nascimento, e_mail, telefone, senha_cliente});
     console.log('Cliente cadastrado:', { imagem, nome, nascimento, e_mail});
-    res.status(201).json({ mensagem: 'Cliente cadastrado com sucesso!' });
+    res.status(201).json({ mensagem: 'Cliente cadastrado com sucesso' });
 });
-
-    app.listen(port, () => {
-  console.log(`App de exemplo está rodando na porta ${port}`);
-});
-
 
 
   app.get("/clientes", async (req, res) => {
+    try {
     const { id } = req.params;
     const clientes = await mysql2.select("*").from("clientes");
     res.json(clientes);
+    } catch (error) {
+      console.log("Erro ao buscar cliente", error);
+      res.status(404).json({ mensagem: 'Cliente não encontrado'});
+  }
   });
 
   
   app.get("/tipo", async (req, res) => {
+    try { 
     const { id } = req.params;
     const tipo = await mysql2.select("*").from("tipo");
     res.json(tipo);
+    } catch (error) {
+      console.log("Erro ao buscar tipo", error);
+      res.status(404).json({mensagem: 'Tipo não encontrado'});
+  }
   });
 
 
   app.get("/bike_Montada", async (req, res) => {
+    try {
     const { id } = req.params;
     const bike_Montada = await mysql2.select("*").from("bike_Montada");
     res.json(bike_Montada);
+    } catch (error) {
+      console.log("Erro ao buscar Bike montada", error);
+      res.status(404).json({mensagem: 'Bike montada não encontrada'});
+  }
   });
 
 
 app.get("/Quadro", async (req, res) => {
+  try {
   const { id } = req.params;
   const Quadro = await mysql2.select("*").from("Quadro");
-  res.json(Quadro)
+  res.json(Quadro);
+  } catch (error) {
+    console.log("Erro ao buscar quadro", error);
+    res.status(404).json({mensagem: 'Quadro não encontrado'});
+}
 });
 
 
 app.get("/acessorios", async (req, res) => {
+  try {
   const { id } = req.params;
   const acessorios = await mysql2.select("*").from("acessorios");
-  res.json(acessorios)
+  res.json(acessorios);
+  } catch (error) {
+    console.log("Erro ao buscar acessório", error);
+    res.status(404).json({mensagem: 'Acessório não encontrado'});
+}
 });
 
 
 app.get("/personalizada", async (req, res) => {
+  try {
   const { id } = req.params;
   const personalizada = await mysql2.select("*").from("personalizada");
-  res.json(personalizada)
+  res.json(personalizada);
+  } catch (error) {
+    console.log("Erro ao buscar Bike personalizada", error);
+    res.status(404).json({mensagem: 'Bike personalizada não encontrada'});
+}
 });
 
 
 app.get("/pedido", async (req, res)  => {
-const { id } = req.params;
+try {
+  const { id } = req.params;
 const pedido = await mysql2.select("*").from("personalizada");
+res.json(pedido);
+} catch (error) {
+  console.log("Erro ao buscar pedido", error);
+  res.status(404).json({mensagem: 'Pedido não encontrado'})
+}
 });
 
 
 app.get("/historico_compras", async (req, res)  => {
+  try {
   const { id } = req.params;
   const historico_compras = await mysql2.select("*").from("historico_compras");
+  res.json(historico_compras);
+  } catch (error) {
+    console.log("Erro ao buscar histórico de compras", error);
+    res.status(404).json({mensagem: 'Histórico não encontrado'});
+  }
   });
 
 
- app.listen(port, () => {
-   console.log(`Example app is running on the port ${port}`)
- });
+app.listen(port, () => {
+  console.log(`App de exemplo está rodando na porta ${port}`);
+});
+
+
